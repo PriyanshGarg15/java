b2b9b4f420b19dd0da9d5824a666d7cedfdfd375
PriyanshGarg15 <116974262+PriyanshGarg15@users.noreply.github.com>
2023-06-24 20:12:11 +0530

    Update world_class_checking.java

--- Access_Modifiers/world_class_checking.java
+++ Access_Modifiers/world_class_checking.java
@@ -1,23 +1,24 @@
-//suppose we are creating this class in package called as Access_modifiers
+//suppose we have package Access_modifiers having file named as subclass.java
 package Access_modifiers;
-
-public class c1 
+public class subclass
 {
     public int x = 5;
     protected int y = 6;
     int z = 7;
     private int a = 8;
 }
-//suppose we had import this class in package called as Strings
-package Strings;
-import Access_modifiers.c1;
 
-public class test {
-    public static void main(String[] args) {
-        c1 c = new c1();
+//suppose we had import this class in package called as Strings having class named as subclass1
+package Strings;
+import Access_modifiers.subclass;
+public class subclass1
+{
+    public static void main(String[] args) 
+    {
+        subclass c=new subclass();
         System.out.println(c.x);
-        // System.out.println(c.y);
-        // System.out.println(c.z);
-        // System.out.println(c.a);
+    //  System.out.println(c.y);
+    //  System.out.println(c.z);
+    //  System.out.println(c.a);
     }
 }

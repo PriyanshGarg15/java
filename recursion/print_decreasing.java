8e2ddc81be87c2e8389a0114fddd486eea6c66a0
PriyanshGarg15 <116974262+PriyanshGarg15@users.noreply.github.com>
2023-06-27 18:49:23 +0530

    Create print_decreasing.java

---
+++ recursion/print_decreasing.java
@@ -0,0 +1,21 @@
+package recursion;
+import java.util.*;
+public class print_decreasing 
+{
+    public static void dec(int n)
+    {
+        if(n==1)
+        {
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        dec(n-1);
+    }
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        dec(n);
+    }
+}

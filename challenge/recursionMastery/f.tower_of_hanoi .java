8270d17e56bf1421c38f676d965a9c0c10b59253
PriyanshGarg15 <116974262+PriyanshGarg15@users.noreply.github.com>
2023-07-25 22:51:30 +0530

    Create f.tower_of_hanoi .java

---
+++ challenge/recursionMastery/f.tower_of_hanoi .java
@@ -0,0 +1,24 @@
+package recursion;
+import java.util.*;
+
+public class tower_of_hanoi 
+{
+    public static void han(int n,char src,char des,char aux) 
+    {
+        if(n==0)
+        {
+            return;
+        }
+        han(n-1,src,aux,des);
+        System.out.println(n+"["+src+"->"+des+"]");
+        han(n-1,aux,des,src);
+
+
+    }
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        han(n,'A','B','C'); 
+    }
+}

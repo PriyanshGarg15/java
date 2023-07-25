bf7a827a06a02093802b83439a5a7b254f08528e
PriyanshGarg15 <116974262+PriyanshGarg15@users.noreply.github.com>
2023-07-26 04:09:07 +0530

    Create k.find_last_occurence_in_array.java

---
+++ challenge/recursionMastery/k.find_last_occurence_in_array.java
@@ -0,0 +1,44 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class find_last_occurence_in_array {
+      public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            int k=sc.nextInt();
+            arr[i]=k;
+        }
+        int d=sc.nextInt();
+        int k=rec(arr,0,d);
+        System.out.println(k);
+    }
+    public static int rec(int arr[],int n,int d) 
+    {
+        if(n==arr.length)
+        {
+            return -1;
+        }
+        int fis=rec(arr,n+1,d);
+        if(fis==-1)
+        {
+            if(arr[n]==d)
+            {
+                return n;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        else
+        {
+            return fis;
+        }
+    }
+    
+}

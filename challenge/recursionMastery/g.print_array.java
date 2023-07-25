ae28e40436fa9b0622a8ccdd51d7301f08874ec1
PriyanshGarg15 <116974262+PriyanshGarg15@users.noreply.github.com>
2023-07-26 00:10:37 +0530

    Create g.print_array.java

---
+++ challenge/recursionMastery/g.print_array.java
@@ -0,0 +1,26 @@
+package recursion;
+import java.util.*;
+public class print_array 
+{
+     public static void rec(int arr[],int n) 
+    {
+        if(n==arr.length)
+        {
+            return;
+        }
+        System.out.println(arr[n]);
+        rec(arr,n+1);
+    }
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            int k=sc.nextInt();
+            arr[i]=k;
+        }
+        rec(arr,0);
+    }
+}

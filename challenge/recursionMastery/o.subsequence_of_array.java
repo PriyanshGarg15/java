f7a8df4fc1a485f1bd08afa77654d9a6e7762d02
PriyanshGarg15 <116974262+PriyanshGarg15@users.noreply.github.com>
2023-08-05 18:43:02 +0530

    Create o.subsequence_of_array.java

---
+++ challenge/recursionMastery/o.subsequence_of_array.java
@@ -0,0 +1,41 @@
+package recursion;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class subsequence
+{
+   public static ArrayList<ArrayList<Integer>> subset(int[] arr)
+   {
+      ArrayList<Integer> curr=new ArrayList<Integer>();
+      ArrayList<ArrayList<Integer>> ans=new ArrayList<ArrayList<Integer>>();
+      subsets(arr,0,curr,ans);
+      return ans;
+
+   }
+   public static void subsets(int[] arr,int i,ArrayList<Integer> curr,ArrayList<ArrayList<Integer>> ans)
+   {
+      if(i==arr.length)
+      {
+         ans.add(new ArrayList<Integer>(curr));
+         return;
+      }
+      curr.add(arr[i]);
+      subsets(arr,i+1,curr,ans);
+      curr.remove(curr.size()-1);
+      subsets(arr,i+1,curr,ans);
+   }
+   public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            int k=sc.nextInt();
+            arr[i]=k;
+        }
+        ArrayList<ArrayList<Integer>> subsets = subset(arr);
+        System.out.println(subsets);
+
+        
+      }
+
+}
